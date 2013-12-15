a5e3d6c063d395e97922eb003fbff46fe114ad17
Carlos Aguilar <caguilar@dwdandsolutions.com>
2013-12-15 00:11:47 -0600

    Adding code for RequestsClient

--- openpay/http_client.py
+++ openpay/http_client.py
@@ -3,6 +3,8 @@ import sys
 import textwrap
 import warnings
 
+from openpay import error, util
+
 # - Requests is the preferred HTTP library
 # - Google App Engine has urlfetch
 # - Use Pycurl if it's there (at least it verifies SSL certs)
@@ -28,12 +30,95 @@ except ImportError:
     urlfetch = None
 
 
+def new_default_http_client(*args, **kwargs):
+    if urlfetch:
+        impl = UrlFetchClient
+    elif requests:
+        impl = RequestsClient
+    elif pycurl:
+        impl = PycurlClient
+    else:
+        impl = Urllib2Client
+        warnings.warn(
+            "Warning: the Openpay library is falling back to urllib2/urllib "
+            "because neither requests nor pycurl are installed. "
+            "urllib2's SSL implementation doesn't verify server "
+            "certificates. For improved security, we suggest installing "
+            "requests.")
+
+    return impl(*args, **kwargs)
+
+
 class HTTPClient(object):
-    pass
+
+    def __init__(self, verify_ssl_certs=True):
+        self._verify_ssl_certs = verify_ssl_certs
+
+    def request(self, method, url, headers, post_data=None):
+        raise NotImplementedError(
+            'HTTPClient subclasses must implement `request`')
 
 
 class RequestsClient(HTTPClient):
-    pass
+    name = 'requests'
+
+    def request(self, method, url, headers, post_data=None, user=None):
+        kwargs = {}
+
+        if self._verify_ssl_certs:
+            kwargs['verify'] = os.path.join(
+                os.path.dirname(__file__), 'data/ca-certificates.crt')
+        else:
+            kwargs['verify'] = False
+
+        try:
+            try:
+                result = requests.request(method,
+                                          url,
+                                          headers=headers,
+                                          data=post_data,
+                                          timeout=80,
+                                          auth=(user, ''),
+                                          ** kwargs)
+            except TypeError, e:
+                raise TypeError(
+                    'Warning: It looks like your installed version of the '
+                    '"requests" library is not compatible with Openpay\'s '
+                    'usage thereof. (HINT: The most likely cause is that '
+                    'your "requests" library is out of date. You can fix '
+                    'that by running "pip install -U requests".) The '
+                    'underlying error was: %s' % (e,))
+
+            # This causes the content to actually be read, which could cause
+            # e.g. a socket timeout. TODO: The other fetch methods probably
+            # are succeptible to the same and should be updated.
+            content = result.content
+
+            status_code = result.status_code
+        except Exception as e:
+            # Would catch just requests.exceptions.RequestException, but can
+            # also raise ValueError, RuntimeError, etc.
+            self._handle_request_error(e)
+        return content, status_code
+
+    def _handle_request_error(self, e):
+        if isinstance(e, requests.exceptions.RequestException):
+            msg = ("Unexpected error communicating with Openpay.  "
+                   "If this problem persists, let us know at "
+                   "support@openpay.mx.")
+            err = "%s: %s" % (type(e).__name__, str(e))
+        else:
+            msg = ("Unexpected error communicating with Openpay. "
+                   "It looks like there's probably a configuration "
+                   "issue locally.  If this problem persists, let us "
+                   "know at support@openpay.mx.")
+            err = "A %s was raised" % (type(e).__name__,)
+            if str(e):
+                err += " with error message %s" % (str(e),)
+            else:
+                err += " with no error message"
+        msg = textwrap.fill(msg) + "\n\n(Network error: %s)" % (err,)
+        raise error.APIConnectionError(msg)
 
 
 class UrlFetchClient(HTTPClient):
