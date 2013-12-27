ed8b3ce664e60ae55e857c1035456a5ad81de497
Carlos Aguilar <caguilar@dwdandsolutions.com>
2013-12-27 12:27:07 -0600

    Adding new tests for resources

--- openpay/test/test_resources.py
+++ openpay/test/test_resources.py
@@ -1,8 +1,11 @@
+import json
 import openpay
 from openpay import util
 
 from openpay.test.helper import (
-    OpenpayUnitTestCase)
+    OpenpayUnitTestCase, OpenpayApiTestCase,
+    MySingleton, MyListable, MyCreatable, MyUpdateable, MyDeletable,
+    MyResource, SAMPLE_INVOICE)
 
 
 class BaseObjectTests(OpenpayUnitTestCase):
@@ -13,3 +16,91 @@ class BaseObjectTests(OpenpayUnitTestCase):
 
         self.assertEqual('foo', obj.id)
         self.assertEqual('bar', obj.api_key)
+
+    def test_access(self):
+        obj = openpay.resource.BaseObject('myid', 'mykey', myparam=5)
+
+        # Empty
+        self.assertRaises(AttributeError, getattr, obj, 'myattr')
+        self.assertRaises(KeyError, obj.__getitem__, 'myattr')
+        self.assertEqual('def', obj.get('myattr', 'def'))
+        self.assertEqual(None, obj.get('myattr'))
+
+        # Setters
+        obj.myattr = 'myval'
+        obj['myitem'] = 'itval'
+        self.assertEqual('sdef', obj.setdefault('mydef', 'sdef'))
+
+        # Getters
+        self.assertEqual('myval', obj.setdefault('myattr', 'sdef'))
+        self.assertEqual('myval', obj.myattr)
+        self.assertEqual('myval', obj['myattr'])
+        self.assertEqual('myval', obj.get('myattr'))
+
+        self.assertEqual(['id', 'myattr', 'mydef', 'myitem'],
+                         sorted(obj.keys()))
+        self.assertEqual(['itval', 'myid', 'myval', 'sdef'],
+                         sorted(obj.values()))
+
+        # Illegal operations
+        self.assertRaises(ValueError, setattr, obj, 'foo', '')
+        self.assertRaises(TypeError, obj.__delitem__, 'myattr')
+
+    def test_refresh_from(self):
+        obj = openpay.resource.BaseObject.construct_from({
+            'foo': 'bar',
+            'trans': 'me',
+        }, 'mykey')
+
+        self.assertEqual('mykey', obj.api_key)
+        self.assertEqual('bar', obj.foo)
+        self.assertEqual('me', obj['trans'])
+
+        obj.refresh_from({
+            'foo': 'baz',
+            'johnny': 5,
+        }, 'key2')
+
+        self.assertEqual(5, obj.johnny)
+        self.assertEqual('baz', obj.foo)
+        self.assertRaises(AttributeError, getattr, obj, 'trans')
+        self.assertEqual('key2', obj.api_key)
+
+        obj.refresh_from({
+            'trans': 4,
+            'metadata': {'amount': 42}
+        }, 'key2', True)
+
+        self.assertEqual('baz', obj.foo)
+        self.assertEqual(4, obj.trans)
+        self.assertEqual({'amount': 42}, obj._previous_metadata)
+
+    def test_refresh_from_nested_object(self):
+        obj = openpay.resource.BaseObject.construct_from(
+            SAMPLE_INVOICE, 'key')
+
+        self.assertEqual(1, len(obj.lines.subscriptions))
+        self.assertTrue(
+            isinstance(obj.lines.subscriptions[0],
+                       openpay.resource.BaseObject))
+        self.assertEqual('month', obj.lines.subscriptions[0].plan.interval)
+
+    def test_to_json(self):
+        obj = openpay.resource.BaseObject.construct_from(
+            SAMPLE_INVOICE, 'key')
+
+        self.check_invoice_data(json.loads(str(obj)))
+
+    def check_invoice_data(self, data):
+        # Check rough structure
+        self.assertEqual(20, len(data.keys()))
+        self.assertEqual(3, len(data['lines'].keys()))
+        self.assertEqual(0, len(data['lines']['invoiceitems']))
+        self.assertEqual(1, len(data['lines']['subscriptions']))
+
+        # Check various data types
+        self.assertEqual(1338238728, data['date'])
+        self.assertEqual(None, data['next_payment_attempt'])
+        self.assertEqual(False, data['livemode'])
+        self.assertEqual('month',
+                         data['lines']['subscriptions'][0]['plan']['interval'])
