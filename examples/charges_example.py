5b57e9c4cf0c898fad92540b58cdff22ea6e228b
Carlos Aguilar <caguilar@dwdandsolutions.com>
2013-12-27 22:45:02 -0600

    Adding sample code for charges

--- examples/charges_example.py
+++ examples/charges_example.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 from os import path, pardir
 PROJECT_ROOT = path.dirname(path.abspath(__file__))
@@ -32,5 +33,35 @@ print charge
 
 print "Get charge with ID: t2chepfmcfhr0uwqqmrp"
 charge = customer.charges.retrieve('t2chepfmcfhr0uwqqmrp')
+print charge
 #charge.capture()
-print charge.refund()
+#print charge.refund()
+
+# print "Creating card as merchant"
+
+# print openpay.Card.create(
+# 	card_number="4111111111111111",
+# 	holder_name="Juan Perez",
+# 	expiration_year="20",
+# 	expiration_month="12",
+# 	cvv2="110",
+# 	address={
+# 		"city":"Querétaro",
+# 		"country_code":"MX",
+# 		"postal_code":"76900",
+# 		"line1":"Av 5 de Febrero",
+# 		"line2":"Roble 207",
+# 		"line3":"col carrillo",
+# 		"state":"Queretaro"
+# 	}
+# )
+# print "Creating charge as merchant"
+# charge = openpay.Charge.create_as_merchant(source_id="k2trvya1nxpcytgww4rt", method="card", amount=100, description="Fourth charge", order_id="oid-00061", capture=False)
+# print charge
+
+print "Retrieve charge with ID: t2uizz6lefdpj20kvucs as merchant"
+charge = openpay.Charge.retrieve_as_merchant("t2uizz6lefdpj20kvucs")
+# print "Capturing charge"
+# print charge.capture(merchant=True)
+print "Refund charge"
+print charge.refund(merchant=True)
