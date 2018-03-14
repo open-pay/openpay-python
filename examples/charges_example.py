8d60f34c223a80c21d5bd2b3756645a9f95ce1d7
Marcos Alvarado <marcos.alvarado@openpay.mx>
2018-03-14 12:46:12 -0600

    Customer charge example

--- examples/charges_example.py
+++ examples/charges_example.py
@@ -9,36 +9,36 @@ import openpay
 openpay.api_key = "sk_10d37cc4da8e4ffd902cdf62e37abd1b"
 openpay.verify_ssl_certs = False
 openpay.merchant_id = "mynvbjhtzxdyfewlzmdo"
-# customer = openpay.Customer.retrieve('amce5ycvwycfzyarjf8l')
-# charges = customer.charges.all()
-# print "\n\nCharges for customer: {0}".format(customer.name)
-# for charge in charges:
-# 	print charge
 
-# merchant_charges = openpay.Charge.as_merchant()
-# print "\n\nCharges as merchant"
-# for charge in merchant_charges:
-# 	print charge
+customer = openpay.Customer.retrieve('amce5ycvwycfzyarjf8l')
+print "customer: ", customer
 
-# print "\n\nRetrieving Charge with ID: t8vgbjfy4vdx3xttigyj"
-# charge = openpay.Charge.retrieve_as_merchant('t8vgbjfy4vdx3xttigyj')
-# print charge
+card = customer.cards.create(
+	card_number="4111111111111111",
+	holder_name="Juan Perez Ramirez",
+	expiration_year="20",
+	expiration_month="12",
+	cvv2="110",
+	address={
+		"city":"Querétaro",
+		"country_code":"MX",
+		"postal_code":"76900",
+		"line1":"Av 5 de Febrero",
+		"line2":"Roble 207",
+		"line3":"col carrillo",
+		"state":"Queretaro"
+   })
 
-# print "\n\nRetrieving Charge for customer {0} with ID: teh5ydydhg4he8ympogf".format(customer.name)
-# charge = customer.charges.retrieve('teh5ydydhg4he8ympogf')
-# print charge
+print "Card: ", card
 
-# print "Creating a charge"
-# charge = customer.charges.create(source_id="kvxvccpsesm4pwmtgnjb", method="card", amount=100, description="Third charge", order_id="oid-00060", capture=False)
-# print charge
+print "Creating card as customer"
+charge = customer.charges.create(source_id=card.id, method="card", amount=100, description="Charge", capture=False)
 
-# print "Get charge with ID: t2chepfmcfhr0uwqqmrp"
-# charge = customer.charges.retrieve('t2chepfmcfhr0uwqqmrp')
-# print charge
-# charge.capture()
-# print charge.refund()
+print "charge: ", charge
 
-# print "Creating card as merchant"
+print charge.refund()
+
+print "Creating card as merchant"
 
 card = openpay.Card.create(
 	card_number="4111111111111111",
@@ -77,16 +77,4 @@ print charge.capture(merchant=True)
 print "Refund charge"
 print charge.refund(merchant=True)
 
-# No hay acceso como merchant para agregar cuentas de banco
-# print "Creating bank account as merchant"
-# account = openpay.BankAccount.create(clabe="012298026516924616", alias="Cuenta principal", holder_name="Carlos Alberto Aguilar")
-# print account
 
-# print "Creating charge as merchant"
-# charge = openpay.Charge.create_as_merchant(method="bank_account", amount=100, description="Fifth charge", order_id="oid-00062")
-# print charge
-
-#print "Retrieve filtered list"
-#charges = openpay.Charge.all(creation={'lte': datetime.datetime.now().strftime('Y-m-d')})
-#charge = charges.retrieve(charges.data[0].id)
-#print type(charge)
